c828f49e51652e9ce7aab03ab5c3f2569c310297
sforcey <96750123+sforcey@users.noreply.github.com>
2021-12-27 20:50:18 -0500

    Update ConvexGeometry.js

--- hedra/ConvexGeometry.js
+++ hedra/ConvexGeometry.js
@@ -1,8 +1,8 @@
 import {
 	BufferGeometry,
 	Float32BufferAttribute
-} from 'three.module.js';
-import { ConvexHull } from 'ConvexHull.js';
+} from './three.module.js';
+import { ConvexHull } from './ConvexHull.js';
 
 class ConvexGeometry extends BufferGeometry {
 
