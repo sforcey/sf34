f234232cd89b4a2093da79a74ffa645fb6daf79b
sforcey <96750123+sforcey@users.noreply.github.com>
2021-12-27 21:50:56 -0500

    Update multihedron.js

--- hedra/multihedron.js
+++ hedra/multihedron.js
@@ -2,6 +2,7 @@ import * as THREE from './three.module.js';
 import { OrbitControls } from './OrbitControls.js';
 import { ConvexGeometry } from './ConvexGeometry.js';
 //import { GLTFExporter } from './GLTFExporter.js';
+import { STLExporter } from './GLTFExporter.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );  //default 75 fov
